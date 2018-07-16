318301ee166eccc385972a428fc897cbad17b49d
Murilo Henrique <murilohns@gmail.com>
2018-07-16 00:03:22 -0300

    add tmux config

--- install/setup.sh
+++ install/setup.sh
@@ -9,12 +9,16 @@ sudo apt-get -qq install silversearcher-ag # Install ag for better FZF usage
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Setup Zsh
-sudo apt-get -qq install zsh #Install zsh
+sudo apt-get -qq install zsh # Install zsh
 chsh -s $(which zsh) # Set zsh as default shell
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" # Install oh-my-zsh on zsh
 
 # Setup Tmux
-sudo apt-get -qq install tmux
+sudo apt-get -qq install tmux # Install tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm # Install Tmux Plugin Manager
+cp ../.tmux.conf ~/ # Move tmux.conf to home
+# You must set tmux source to ~/.tmux.conf running the following command: tmux source ~/.tmux.conf
+# Then you must go to ~/ and run: tmux, then press (Ctrl + b) + I
 
 # Run script to install ubuntu packages
 source ~/.dotfiles/install/ubuntu.sh
