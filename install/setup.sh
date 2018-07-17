2da400825150edb5455bccd223e3d16a55e4f486
Murilo Henrique <murilohns@gmail.com>
2018-07-16 23:40:57 -0300

    add docker setup

--- install/setup.sh
+++ install/setup.sh
@@ -20,6 +20,20 @@ cp ../.tmux.conf ~/ # Move tmux.conf to home
 echo "You must set tmux source to ~/.tmux.conf running the following command: tmux source ~/.tmux.conf"
 echo " Then you must go to ~/ and run: tmux, then press (Ctrl + b) + I to install the extensions"
 
+# Setup Docker
+apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
+apt-get update
+apt-get install docker-ce
+
+curl -L https://github.com/docker/compose/releases/download/1.20.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+groupadd docker
+usermod -aG docker $USER
+chmod 777 /var/run/docker.sock
+
 # Run script to install ubuntu packages
 source ~/.dotfiles/install/ubuntu.sh
 
